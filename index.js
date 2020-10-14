276de7a7d42ba11ff9212522fbdca7ef25a59177
Alex <67952753+fennyflop@users.noreply.github.com>
2020-10-14 10:29:49 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -2,32 +2,34 @@
 
 setInterval(() => {
     const messages = document.getElementsByName('content');
-const message = messages[0];
-const text = 'Hello, i am a bot. This action was performed automatically';
-const submitButton = document.querySelector('.btn-round');
+    const message = messages[0];
+    const text = 'Hello, i am a bot. This action was performed automatically';
+    const submitButton = document.querySelector('.btn-round');
 
-message.value = text;
-submitButton.click();
+    message.value = text;
+    submitButton.click();
 }, 5000);
 
 // Поднятие заказов
 
-const openForm = () => {
-    const raiseButton = document.querySelector('.js-lot-raise');    
-    raiseButton.click();
-};
-openForm();
-const checkEverything = () => {
-    const checkboxes = document.querySelectorAll("input[type='checkbox']");
-    checkboxes.forEach((item) => {
-        if(!item.checked){
-            item.click();
-        }
-    })
-};
-openForm();
-setTimeout(function () {
-    checkEverything();
-    const submit = document.querySelector('.js-lot-raise-ex');
-    submit.click();
-}, 1000);
+setInterval(function () {
+    const openForm = () => {
+        const raiseButton = document.querySelector('.js-lot-raise');    
+        raiseButton.click();
+    };
+    openForm();
+    const checkEverything = () => {
+        const checkboxes = document.querySelectorAll("input[type='checkbox']");
+        checkboxes.forEach((item) => {
+            if(!item.checked){
+                item.click();
+            }
+        })
+    };
+    openForm();
+    setTimeout(function () {
+        checkEverything();
+        const submit = document.querySelector('.js-lot-raise-ex');
+        submit.click();
+    }, 1000);
+}, 1000)     
